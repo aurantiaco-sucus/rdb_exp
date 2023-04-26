4463edc9164a22ebb04a7203adcd92f1bf0ff3cd
AurantiacoSucus <30406783+aurantiaco-sucus@users.noreply.github.com>
2023-04-26 17:08:37 +0800

    23.4.26-1

--- exp1.sql
+++ exp1.sql
@@ -1,9 +1,13 @@
+drop table SC;
+drop table Course;
+drop table Student;
+
 CREATE TABLE Student
 (
     Sno    varchar2(17) primary key,
     Sname  varchar2(10) not null,
     Sage   integer,
-    Ssex   char(2),
+    Ssex   char(4),
     Sdept  varchar2(20)
 );
 
@@ -48,31 +52,12 @@ values ('S0001', 'C0003', 78);
 
 alter table Student add Senrollment date;
 
-update Student
-set Senrollment='01-9月-2010'
-where Sno='S0001';
-update Student
-set Senrollment='02-10月-2011'
-where Sno='S0002';
-update Student
-set Senrollment='03-11月-2012'
-where Sno='S0003';
-
 alter table Student add check ( Ssex in ('男', '女') );
 
 alter table Course add Cdesc varchar2(200);
-
-update Course
-set Cdesc='高等的数学'
-where Cno='C0001';
-update Course
-set Cdesc='概率的论'
-where Cno='C0002';
-update Course
-set Cdesc='数理的统计'
-where Cno='C0003';
 
 alter table Course modify Cdesc varchar2(500);
 
 alter table Course drop column Cdesc;
 
+alter table Course add foreign key (Cpno) references Course (Cno)
